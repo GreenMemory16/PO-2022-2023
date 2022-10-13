4ed821b35d91e3b93fc750e5f834ad0f0fda8aa5
wisewizardofthestars <sara.pinheiro@tecnico.ulisboa.pt>
2022-10-13 12:17:54 +0100

    more useless stuff i have some time to do;

---
+++ prr-core/src/prr/terminals/Basic.java
@@ -0,0 +1,13 @@
+package prr.terminals;
+import java.io.Serializable;
+
+public class Basic extends Terminal {
+
+    
+    //no extra atributes~
+    //no extra functions
+    //the type of object is what matters
+    public Basic(String id, String clientkey){
+        super(String id, String clientkey);
+    }
+}
--- prr-core/src/prr/terminals/Fancy.java
+++ prr-core/src/prr/terminals/Fancy.java
@@ -3,8 +3,10 @@ import java.io.Serializable;
 
 public class Fancy extends Terminal {
 
-
+    
     //no extra atributes~
+    //no extra functions
+    //the type of object is what matters
     public Fancy(String id, String clientkey){
         super(String id, String clientkey);
     }
