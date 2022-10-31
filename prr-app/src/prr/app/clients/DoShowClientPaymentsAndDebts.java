7af0ef41825544ed3cc80a0675b9f898323e66da
wisewizardofthestars <sara.juliao24@gmail.com>
2022-10-31 21:54:34 +0000

    clients menu dones

--- prr-app/src/prr/app/clients/DoShowClientPaymentsAndDebts.java
+++ prr-app/src/prr/app/clients/DoShowClientPaymentsAndDebts.java
@@ -1,10 +1,11 @@
 package prr.app.clients;
 
+import prr.clients.Client;
 import prr.Network;
+import prr.exceptions.UnknownClientKeyExceptionCore;
 import prr.app.exceptions.UnknownClientKeyException;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME add more imports if needed
 
 /**
  * Show the payments and debts of a client.
@@ -13,11 +14,18 @@ class DoShowClientPaymentsAndDebts extends Command<Network> {
 
 	DoShowClientPaymentsAndDebts(Network receiver) {
 		super(Label.SHOW_CLIENT_BALANCE, receiver);
-		//FIXME add command fields
+		addStringField("key", Prompt.key());
 	}
 
 	@Override
 	protected final void execute() throws CommandException {
-                //FIXME implement command
+		try {
+			Client client = (Client) _receiver.getClient(stringField("key"));
+        	//_display.popup(_receiver.getClient(stringField("key")));
+			_display.popup(Message.clientPaymentsAndDebts("key", client.Payments(), client.Debts()));
+		}
+		catch (UnknownClientKeyExceptionCore e) {
+			throw new UnknownClientKeyException(e.getKey());
+		}
 	}
-}
+	}
